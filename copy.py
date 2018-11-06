ae46bf7f2ec710e91cfab30e2a84e136cf9f1db3
Unknown <nd8380@student.uni-lj.si>
2018-11-06 10:24:14 +0100

    Small fix in copy script & widget-catalog icons not showing fix.

--- copy.py
+++ copy.py
@@ -1,6 +1,7 @@
 import glob
 import shutil
 import json
+import os
 
 """
 Generates string of values which is later added to .md files.
@@ -22,7 +23,10 @@ def add_front_matter(filename,widget):
         content = f.read()
         for element in widget:
             if 'file' not in element:
-                front_matter += '"'+element+'" = "' + str(widget[element]) + '"\n'
+                if element == 'icon':
+                    front_matter += '"' + element + '" = "icons/' + str(widget[element].split('/')[-1]) + '"\n'
+                else:
+                    front_matter += '"'+element+'" = "' + str(widget[element]) + '"\n'
         front_matter += "+++\n"
         front_matter += content
     return front_matter
@@ -62,28 +66,33 @@ def build_json(filename):
 
 
 # find widgets_data.json file in submodule
-files = glob.glob('external/**/widgets_data.json', recursive=True)
+files = glob.glob('external/**/widget_data.json', recursive=True)
 
 # if no file found, exit
 if len(files) < 1:
-    print("Can't find widgets_data.json file.")
+    print("Can't find widget_data.json file.")
     exit()
 json_content = {}
 
-path = files[0].split("widgets_data")[0]
+path = files[0].split("widget_data")[0]
 file = files[0]
 location = "content/widget-catalog/"
 
 widgets_data = build_json(file)
 
-# open widgets_data.json file
+# open widget_data.json file
 with open(file, 'r+') as f:
     widgets = json.loads(f.read())
-    # for each widget in widgets_data.json file generate FrontMatter and copy .md file and icon image
+    # for each widget in widget_data.json file generate FrontMatter and copy .md file and icon image
     for widget in widgets:
         widget_data = widgets[widget]
         text = add_front_matter(path+widget_data['file'], widgets_data[widget_data['title']])
         with open(location+widget_data['file'].split("/")[-1], 'w') as tmp:
             tmp.write(text)
             tmp.close()
-        shutil.copy2(path + widget_data['icon'], 'static/icons/')
+        image_path = "static/"+'/'.join(widget_data['icon'].split('/')[:-1])+"/"
+        try:
+            shutil.copy2(path + widget_data['icon'], image_path)
+        except IOError as e:
+            os.makedirs(image_path)
+            shutil.copy2(path + widget_data['icon'], image_path)
