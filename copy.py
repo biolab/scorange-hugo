60f04f7365af39c64db2ecaec5a6febdaf79ee25
Unknown <nd8380@student.uni-lj.si>
2018-11-16 14:20:19 +0100

    Copy script updated & submodule added.

--- copy.py
+++ copy.py
@@ -2,6 +2,8 @@ import glob
 import shutil
 import json
 import os
+import re
+
 
 """
 Generates string of values which is later added to .md files.
@@ -22,14 +24,37 @@ def add_front_matter(filename,widget):
     with open(filename, 'r+') as f:
         content = f.read()
         for element in widget:
-            if 'file' not in element:
+            if 'url' in element:
+                continue
+            elif 'file' not in element:
                 if element == 'icon':
                     front_matter += '"' + element + '" = "icons/' + str(widget[element].split('/')[-1]) + '"\n'
                 else:
                     front_matter += '"'+element+'" = "' + str(widget[element]) + '"\n'
         front_matter += "+++\n"
-        front_matter += content
+        front_matter += content.replace("images/", "/images/")
     return front_matter
+
+
+def copy_images(path,filename):
+    # line = '![](images/'
+    # p = re.compile("images/")
+    # content = f.read()
+    with open(path+filename, 'r+') as f:
+        content = f.read()
+
+        regex = "images/(.*?)\)"
+        locationFrom = path+"/widgets/images/"
+        locationTo = "static/images/"
+        matches = re.findall(regex,content)
+        # print(matches)
+        for match in matches:
+            try:
+                shutil.copy2(locationFrom+match, locationTo+match)
+            except IOError as e:
+                folders = '/'.join(match.split("/")[:-1])
+                os.makedirs(locationTo+folders)
+                shutil.copy2(locationFrom+match, locationTo+match)
 
 
 """
@@ -53,6 +78,10 @@ def build_json(filename):
             if widget_obj["category"] not in json_content:
                 json_content[widget_obj["category"]] = []
             json_content[widget_obj["category"]].append(widget_obj)
+            # print("---",widget_obj['title'],"---")
+            widget_obj['url'] = url = widget_obj['title'].lower().replace(" ", "_")
+        # front_matter += '"url" = "' + title + '"\n'
+        # print(title)
 
     with open('data/widgets.json', 'w') as outfile:
         json.dump(json_content, outfile)
@@ -87,6 +116,7 @@ with open(file, 'r+') as f:
     for widget in widgets:
         widget_data = widgets[widget]
         text = add_front_matter(path+widget_data['file'], widgets_data[widget_data['title']])
+        copy_images(path,widget_data['file'])
         with open(location+widget_data['file'].split("/")[-1], 'w') as tmp:
             tmp.write(text)
             tmp.close()
